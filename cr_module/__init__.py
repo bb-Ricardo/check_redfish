009ef2a2293a38e9a3dcd33790a8692daac785e8
Ricardo Bartels <ricardo.bartels@telekom.de>
2025-07-26 11:28:29 +0200

    improves logic to discover booting iLO 6 system #170

--- cr_module/__init__.py
+++ cr_module/__init__.py
@@ -13,6 +13,8 @@ from cr_module.classes.plugin import PluginData
 system_power_state = None
 system_power_on_time = None
 system_power_on_time_discovered = False
+system_booting = False
+system_booting_discovered = False
 
 system_boot_max_seconds = 300
 
@@ -61,10 +63,30 @@ def get_system_power_on_time():
 
 def system_is_booting():
 
+    global system_booting, system_booting_discovered
+
+    if system_booting_discovered is True:
+        return system_booting
+
+    plugin_object = PluginData()
+
+    for system in plugin_object.rf.get_system_properties("systems") or list():
+        # used by HPE iLO 6
+        system_discovered = grab(
+            plugin_object.rf.get(system),
+            f"Oem.{plugin_object.rf.vendor_dict_key}.DeviceDiscoveryComplete.ServerFirmwareInventoryComplete")
+
+        if system_discovered is True:
+            system_booting_discovered = True
+            system_booting = False
+            return system_booting
+
     power_on_time = get_system_power_on_time()
     if power_on_time is not None and power_on_time <= system_boot_max_seconds:
-        return True
+        system_booting = True
 
-    return False
+    system_booting_discovered = True
+
+    return system_booting
 
 # EOF
